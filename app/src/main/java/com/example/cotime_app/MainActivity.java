deda58863f25c22bfb676229fd2b21ab998704d0
Aman Rahguvanshi <amanraghuvanshi651@gmail.com>
2021-02-09 13:02:13 +0530

    splash screen added

--- app/src/main/java/com/example/cotime_app/MainActivity.java
+++ app/src/main/java/com/example/cotime_app/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.cotime_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View decorView =getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        |View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        );
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, OnBoardingActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
     }
 }
